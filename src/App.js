0252a13daeaa5cc9a768c405ab25d00b7eb5b0a2
smaranika-panda <smaranikapanda603@gmail.com>
2023-07-08 22:05:32 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import Home from './components/Home';
 import './App.css';
+import { Routes,  Route, BrowserRouter } from "react-router-dom"
+import Lpackage from './components/landing_package/Lpackage'
+import Contact from './components/contact/Contact'
+import Blog from './components/blog/Blog'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+       <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/blog' element={<Blog/>}/> 
+        <Route exact path='/contact' element={<Contact/>}/>
+        <Route exact path='/package' element={<Lpackage/>}/>
+       
+        </Routes>
+        </BrowserRouter>
+    </>
   );
 }
 
